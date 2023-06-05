c50aa0cf51b6ed0555f9c965f55a8699964e4f68
frvnci5 <frvncis@outlook.com>
2023-06-05 10:58:07 -0700

    added time divs to hmtl, added dayjs and click to save button in js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,41 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var currentDay = $('#currentDay');
+var currentDayEl = $('#currentDay');
+// TODO: Add code to display the current date in the header of the page.
+currentDayEl.text(dayjs().format('dddd, MMMM DD h:mma'));
 var past = $('.past');
 var present = $('.present');
 var future = $('.future');
-$(function () {
+var currentHour = dayjs().format('h');
+console.log(currentHour);
+
+
+$(document).ready(function(){
+  $('.saveBtn').on('click', function(){
+    var textAreaEl = $(this).siblings('.description').val();
+    console.log(textAreaEl);
+    var currentTime = $(this).parent().attr('id');
+    console.log(currentTime);
+    localStorage.setItem(currentTime, textAreaEl);
+  })
+});
+
+function hourTracker() {
+  $('.time-block').each(function() {
+    var currentHourCheck = parseInt($(this).attr('id').split('hour'));
+   if (currentHourCheck < currentHour) {
+    $(this).removeClass('present future').addClass('past');
+   } else if (currentHourCheck === currentHour) {
+    $(this).removeClass('past future').addClass('present');
+   } else 
+    $(this).removeClass('past present').addClass('future');
+  });
+  
+}
+hourTracker();
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -23,5 +53,4 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
-});
+
