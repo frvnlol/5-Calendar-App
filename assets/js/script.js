484c841266dd3228d2ef777b009827de83052867
frvnci5 <frvncis@outlook.com>
2023-06-05 16:26:37 -0700

    cleaned up JS format

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,32 +3,32 @@ var currentDayEl = $("#currentDay");
 currentDayEl.text(dayjs().format("dddd, MMMM DD h:mma"));
 
 // Current hour
-var currentHour = dayjs().hour()
+var currentHour = dayjs().hour();
 
 // Add click listener to save button and sets the text area input into local storage.
-$(".saveBtn").on("click", function(){
+$(".saveBtn").on("click", function () {
   var textAreaEl = $(this).siblings(".description").val();
   var currentTime = $(this).parent().attr("id");
   localStorage.setItem(currentTime, textAreaEl);
 });
 
-$(".time-block").each(function(){
+$(".time-block").each(function () {
   // Checks hour of each time block
   var currentHourCheck = $(this).attr("id").split("hour-")[1];
   // Compares current hour to hour on time block, adds / removes classes, then displays correct color.
   if (currentHourCheck < currentHour) {
-    $(this).removeClass('present future').addClass("past");
+    $(this).removeClass("present future").addClass("past");
   } else if (currentHourCheck == currentHour) {
-    $(this).removeClass('past future').addClass("present");
-  } else { 
-    $(this).removeClass('past present').addClass("future");
-}
+    $(this).removeClass("past future").addClass("present");
+  } else {
+    $(this).removeClass("past present").addClass("future");
+  }
 });
 
 // Gets data from local storage to display in workday text area.
-function getFromLocal(){
-for (var i = 9; i <= 17; i++){
-  $('#hour-' + i + ' .description').val(localStorage.getItem('hour-' + i));
+function getFromLocal() {
+  for (var i = 9; i <= 17; i++) {
+    $("#hour-" + i + " .description").val(localStorage.getItem("hour-" + i));
+  }
 }
-};
 getFromLocal();
